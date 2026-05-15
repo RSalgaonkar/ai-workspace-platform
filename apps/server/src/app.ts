608b344c16e6dc9782914e3303fe3219624bbf66
Rashmith Salgaonkar <salgaonkar.rashmith.com>
2026-05-16 00:51:22 +0530

    upload size changes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -29,7 +29,18 @@ app.use(helmet());
 
 app.use(morgan("dev"));
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "50mb"
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "50mb"
+  })
+);
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
